7a4f5678ac5c857081445a5f71db63793acd714a
TomNdex <tomndebele@hotmail.co.uk>
2021-04-04 16:07:52 +0100

    some refactoring done

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -5,11 +5,12 @@ Please have the following information available when you start - 1) Price of Hou
                                                                  3) Deposit Required
                                                                  4) Length of Mortgage""")
 
-price_of_house = float(input("How much does the house cost?: $"))
+price_of_house = float(input("How much does the house cost?: £"))
 # have decided to let user know not to include the % symbol as im not sure how i would be able to only use the number without the symbol if it is not seperated by space
 deposit = float(input("How much is the required deposit? (dont include percentage symbol) "))
 fixed_annual_interest_rate = float(input("What is the fixed annual interest rate? (dont include percentage symbol) "))
 length_of_mortgage = int(input("How long is the mortgage supposed to run for? "))
+print(" ")
 
 principal = price_of_house - (deposit/100*price_of_house)
 interest_rate = (fixed_annual_interest_rate/100)/12
@@ -20,7 +21,9 @@ def monthly_mortgage_payment(principal, interest_rate, number_of_payments):
     total_amaount_paid = (single_payment * number_of_payments)
     total_interest_paid = (total_amaount_paid - principal)
     print("The total monthly payment is £"+str(single_payment))
+    print(" ")
     print("The total amount paid over the course of the whole mortgage is £"+str(total_amaount_paid))
+    print(" ")
     print("The total interest paid is £"+str(total_interest_paid))
 
 monthly_mortgage_payment(principal, interest_rate, number_of_payments)
